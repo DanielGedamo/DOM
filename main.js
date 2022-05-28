9aec8d6a3d70638e3b7e5d7cadb45c39456934fe
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-28 19:22:35 +0300

    c1

---
+++ main.js
@@ -0,0 +1,17 @@
+// console.log(myId.innerText);
+
+// interP.innerText = ("my dom app ")
+
+// var classtext =document.getElementsByClassName("enter1")
+
+// console.log(classtext);
+// console.log(classtext[1]);
+// console.log(classtext[2].innerText);
+
+// var classForMe = document.getElementsByClassName("for elaments")
+// console.log(classForMe);
+
+function getName() {
+    var userName = prompt('enter a name please')
+    document.write('<h2>'+userName +'</h2>')
+} ;getName()
